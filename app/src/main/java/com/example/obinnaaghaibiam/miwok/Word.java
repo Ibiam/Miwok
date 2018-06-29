b9d2f7d9f4e369ea464188527e6a3aee5ba88acc
[Obinna Ibiam] <[ibiamobinna@yahoo.com]>
2018-06-29 16:09:28 +0100

    Initial commit is for a bonus course I did while doing Android Basics @Udacity

---
+++ app/src/main/java/com/example/obinnaaghaibiam/miwok/Word.java
@@ -0,0 +1,104 @@
+package com.example.obinnaaghaibiam.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+
+public class Word {
+    /**
+     * Default translation for the word
+     */
+    private String mDefaultTranslation;
+    /**
+     * Miwok translation for the word
+     */
+    private String mMiwokTranslation;
+    /**
+     * Miwok image resource for the word
+     */
+    private int mImageResourceId;
+    /**
+     * Miwok audio resource for the word
+     */
+    private int mAudioResourceId;
+//    /**
+//     * Constant value that represents no image was provided for this word
+//     */
+//    public final static int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * Create a new Word object.
+     *  @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     * @param imageResourceId    is the word in the Miwok language
+     * @param audioResourceId    is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Create a new Word object.
+     *  @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     * @param audioResourceId    is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    /**
+     * Get the Miwok image resource of the word.
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Get the Miwok audio resource of the word.
+     */
+    public int getmAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
+    }
+
+    //    /**
+//     * Returns whether or not there is an image for this word.
+//     */
+//    public boolean hasImage() {
+//        return mImageResourceId != NO_IMAGE_PROVIDED;
+//    }
+
+
+}
---
+++ app/src/main/java/com/example/obinnaaghaibiam/miwok/WordAdapter.java
@@ -0,0 +1,111 @@
+package com.example.obinnaaghaibiam.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.text.style.BackgroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * This is our own custom constructor (it doesn't mirror a superclass constructor).
+ * The context is used to inflate the layout file, and the list is the data we want
+ * to populate into the lists.
+ */
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    /**
+     * Resource ID for the background color for this list of words
+     */
+    private int mColorResourceId;
+
+    // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+    // the second argument is used when the ArrayAdapter is populating a single TextView.
+    // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+    // going to use this second argument, so it can be any value. Here, we used 0.
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+
+        /**
+         * Provides a view for an AdapterView (ListView, GridView, etc.)
+         *
+         * @param position The position in the list of data that should be displayed in the
+         *                 list item view.
+         * @param convertView The recycled view to populate.
+         * @param parent The parent ViewGroup that is used for inflation.
+         * @return The View for the position in the AdapterView.
+         */
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_textView);
+        // Get the Miwok translation of number from the current Word object and
+        // set this text on the miwok TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_textView);
+        // Get the English translation of number from the current Word object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID list_item_image
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.list_item_image);
+        // Get the image resource ID from the current Word object and
+        // Check if an image is provided for this word or not
+        if (currentWord.getImageResourceId() != 0) {
+            imageView.setImageResource(currentWord.getImageResourceId());
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        View playicon = listItemView.findViewById(R.id.play);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+        playicon.setBackgroundColor(color);
+
+
+//        if (currentWord.hasImage()) {
+//            // If an image is available, display the provided image based on the resource ID
+//            imageView.setImageResource(currentWord.getImageResourceId());
+//            // Make sure the view is visible
+//            imageView.setVisibility(View.VISIBLE);
+//        } else {
+//            // Otherwise hide the ImageView (set visibility to GONE)
+//            imageView.setVisibility(View.GONE);
+//        }
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+
+    }
+}
